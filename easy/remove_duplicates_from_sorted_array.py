9b5fc5ba1b6c91b44c725afd8bbb4db1762a2fbc
Alexandr Vagner <sanyapalmero@gmail.com>
2022-09-21 16:31:48 +0500

    Solve 'Remove Duplicates from Sorted Array'

---
+++ easy/remove_duplicates_from_sorted_array.py
@@ -0,0 +1,9 @@
+def remove_duplicates(nums: list) -> int:
+    k = 1
+
+    for i in range(1, len(nums)):
+        if nums[i - 1] != nums[i]:
+            nums[k] = nums[i]
+            k += 1
+
+    return k
